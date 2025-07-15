91b5444bfd88eb30cbaa034bf3409bacc81599b6
gowthamchi <130386327+gowthamchi@users.noreply.github.com>
2025-07-15 13:39:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from Node.js App!'));
+app.get('/', (req, res) => res.send('Hello from Node.js App! this is gowtham reddy signing off!!!'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
