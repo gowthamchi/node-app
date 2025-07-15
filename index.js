286e2a23b263de18f0e0df9b4e0027758185bba8
gowthamchi <130386327+gowthamchi@users.noreply.github.com>
2025-07-15 11:29:02 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello from Node.js App!'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
